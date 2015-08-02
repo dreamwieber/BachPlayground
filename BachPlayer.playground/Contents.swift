07da6d67f52cee81f73868231b1a3f864c3d620f
dreamwieber <gregorywieber@gmail.com>
2015-08-01 20:06:33 -0700

    initial commit

---
+++ BachPlayer.playground/Contents.swift
@@ -0,0 +1,35 @@
+//  Bach MIDI File from http://www.jsbach.net/midi/midi_goldbergvariations.html
+
+import UIKit
+import AVFoundation
+import XCPlayground
+
+XCPSetExecutionShouldContinueIndefinitely(true)
+
+let audioEngine = AVAudioEngine()
+let mixer = audioEngine.mainMixerNode
+let sampler: AVAudioUnitSampler = AVAudioUnitSampler()
+
+audioEngine.attachNode(sampler)
+audioEngine.connect(sampler, to: mixer, format: nil)
+
+let midiFile = NSBundle.mainBundle().URLForResource("bach", withExtension: "mid")!
+var midiPlayer: AVMIDIPlayer?
+
+do {
+    try midiPlayer = AVMIDIPlayer(contentsOfURL: midiFile, soundBankURL: nil)
+    midiPlayer?.prepareToPlay()
+} catch {
+    print("could not create MIDI player")
+}
+
+do {
+    try audioEngine.start()
+    midiPlayer?.play {
+        print("finished playing")
+    }
+} catch {
+    print("could not start audio engine")
+}
+
+// midiPlayer?.stop()
