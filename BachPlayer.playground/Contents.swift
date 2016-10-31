59479d941696fe85a35b58eecde8cb0a19fff2e8
Aurelius Prochazka <aure@users.noreply.github.com>
2016-10-31 13:31:13 -0700

    Updated for Swift 3

--- BachPlayer.playground/Contents.swift
+++ BachPlayer.playground/Contents.swift
@@ -1,38 +1,22 @@
 //  Bach MIDI File from http://www.jsbach.net/midi/midi_goldbergvariations.html
 
-import UIKit
 import AVFoundation
-import XCPlayground
+import PlaygroundSupport
 
-XCPSetExecutionShouldContinueIndefinitely(true)
+PlaygroundPage.current.needsIndefiniteExecution = true
 
-let audioEngine = AVAudioEngine()
-let mixer = audioEngine.mainMixerNode
-
-// Sampler not actually used to play MIDI,
-// but AVAudioEngine needs at least one input
-let sampler: AVAudioUnitSampler = AVAudioUnitSampler()
-
-audioEngine.attachNode(sampler)
-audioEngine.connect(sampler, to: mixer, format: nil)
-
-let midiFile = NSBundle.mainBundle().URLForResource("bach", withExtension: "mid")!
+let midiFile = Bundle.main.url(forResource: "bach", withExtension: "mid")!
 var midiPlayer: AVMIDIPlayer?
 
 do {
-    try midiPlayer = AVMIDIPlayer(contentsOfURL: midiFile, soundBankURL: nil)
+    try midiPlayer = AVMIDIPlayer(contentsOf: midiFile, soundBankURL: nil)
     midiPlayer?.prepareToPlay()
 } catch {
     print("could not create MIDI player")
 }
 
-do {
-    try audioEngine.start()
-    midiPlayer?.play {
-        print("finished playing")
-    }
-} catch {
-    print("could not start audio engine")
+midiPlayer?.play {
+    print("finished playing")
 }
 
-midiPlayer?.stop()
+//midiPlayer?.stop()
